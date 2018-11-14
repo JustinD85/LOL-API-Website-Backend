f9e2440e72ba1fa7e7ef1a62959ca7d31f1911a7
Ubuntu <ubuntu@ip-172-31-0-40.us-east-2.compute.internal>
2018-11-14 16:04:23 +0000

    new route

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./client/build/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+app.listen(80);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,10 +18,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static((`${__dirname}/../build`)));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.get('/',(req,res)=>{
+	res.sendFile(path.join(__dirname, './client/build/index.html'))
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
