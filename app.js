669abfc44e0ca512955d17e40363311eb054620c
Justin <justin@baal-software.com>
2018-11-14 14:30:25 -0700

    hmmm

--- app.js
+++ app.js
@@ -1,39 +1,24 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const axios = require('axios');
+const apiKey = `api_key=RGAPI-6c4f0334-3d97-482b-a5f6-dc71bf0b62a0`;
+const riotUrl = `https://na1.api.riotgames.com/lol/`;
+app = express();
 
-//var indexRouter = require('./client/build/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-app.use('/users', usersRouter);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', function (req, res, next) {
+  let summonerName = `${riotUrl}summoner/v3/summoners/by-name/${req.query.name}?${apiKey}`;
+  axios(summonerName)
+  .then(info => {
+    const matchOpt = `beginIndex=${0}&endIndex=${1}&`;
+    const matchList = `${riotUrl}match/v3/matchlists/by-account/${info.data.accountId}?${matchOpt}${apiKey}`
+    return axios(matchList)
+    }).then(thatInfo => {
+      const match = `match/v3/matches/${thatInfo.data.matches[0].gameId}?`;
+      return axios(`${riotUrl}${match}${apiKey}`)
+    }).then(e => {
+      console.log(typeof e.data);
+      res.json(
+        e.data
+      );
+    }).catch(e => console.log(e))
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(3131, ()=> console.log('We on: 3131 😎'))
