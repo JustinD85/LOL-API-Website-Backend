8bf4277c26a1b3c4d204a356bdd7759b694b045c
Justin <justin@baal-software.com>
2018-11-14 13:49:35 -0700

    errurs

---
+++ client/build/precache-manifest.d74cab87fdac3d931c11ce748b6dfb8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "874bef5087e8151a36d9",
+    "url": "/static/js/main.874bef50.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "874bef5087e8151a36d9",
+    "url": "/static/css/main.4fc16a27.chunk.css"
+  },
+  {
+    "revision": "1c08d15442645b0e872e8f1dbe4d442d",
+    "url": "/index.html"
+  }
+];
