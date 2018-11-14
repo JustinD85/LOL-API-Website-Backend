b8aeff2f2777efe2009fa360d8b4e101a9171a19
Justin <justin@baal-software.com>
2018-11-14 09:08:26 -0700

    send build

---
+++ client/build/precache-manifest.f71886bba8d30ce5b6b6eb989b1c110b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3c61389ab38c71abb14b",
+    "url": "/static/js/main.3c61389a.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "3c61389ab38c71abb14b",
+    "url": "/static/css/main.4fc16a27.chunk.css"
+  },
+  {
+    "revision": "1de23b14569edc89acb97db7b90979c0",
+    "url": "/index.html"
+  }
+];
